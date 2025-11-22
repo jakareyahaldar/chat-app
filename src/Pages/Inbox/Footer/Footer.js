24687e3272540fb9442cf5896e57b6020960f9ac
Sagor20005 <ms6392883@gmail.com>
2025-11-22 09:55:00 +0600

    add disconnect alert on send message

--- src/Pages/Inbox/Footer/Footer.js
+++ src/Pages/Inbox/Footer/Footer.js
@@ -1,11 +1,12 @@
 import { socket } from "../../../socket.js"
 import { useState, useRef } from "react"
 import { useSelector } from "react-redux"
+import MyAlert from "../../../CastomElements/MyAlert.js"
 
 export default function Footer({ chat_id, members, my_id }){
   const InputEl = useRef({})
   const helper = useSelector((state)=> state.helper)
-  console.log(helper)
+  const [showAlert,setShowAlert] = useState(false)
   const messageTemplate = {
     message_id: window.crypto.randomUUID(),
     sender: my_id,
@@ -20,7 +21,7 @@ export default function Footer({ chat_id, members, my_id }){
   function sendMessage(){
     InputEl.current.focus()
     if(!helper.socketIo_connected){
-      alert("Cant Send message!")
+      setShowAlert(true)
       return
     }
     socket.emit("user_message",{ ...message, atSend:Date.now() })
@@ -30,7 +31,22 @@ export default function Footer({ chat_id, members, my_id }){
   
   
   return(
-    <div className=" shrink-0 flex items-center justify-between p-3 px-5">
+    <>
+      
+      {/*Alert*/}
+      <MyAlert 
+        title="Error!"
+        text="Server Disconnected"
+        show={showAlert}
+        fixed={true}
+        yes_btn_text="Ok"
+        no_btn_text="Cancel"
+        onClose={()=>setShowAlert(false)}
+        onConfrom={()=>setShowAlert(false)}
+      />
+      
+      {/*Main Jsx*/}
+      <div className=" shrink-0 flex items-center justify-between p-3 px-5">
       {/*Options*/}
       <div className="text-2xl flex gap-3">
         <i className="fa-solid fa-image"></i>
@@ -48,5 +64,6 @@ export default function Footer({ chat_id, members, my_id }){
         <i onClick={sendMessage} className="text-2xl fa fa-paper-plane" aria-hidden="true"></i>
       </div>
     </div>
+    </>
     )
 }
