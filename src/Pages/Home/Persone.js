96b60d07ec2a9b8fc90041ebaff257f732aacaba
Sagor20005 <ms6392883@gmail.com>
2025-12-10 11:05:58 +0600

    .

--- src/Pages/Home/Persone.js
+++ src/Pages/Home/Persone.js
@@ -18,7 +18,7 @@ export default function Persone({ last_message, chat }){
           <img className="w-[70px] h-[70px] rounded-full shrink-0 object-cover" src={user_avatar ? user_avatar : defaultAvatar} alt="Persone" />
           <div className="flex flex-col ">
             <p>{user_name}</p>
-            <p className={`${!last_message.seen && !isMyMessage ? "font-bold" :"text-[#84d9d5]"} text-sm`}>{typing || last_message_text }</p>
+            <p className={`${!last_message.seen && !isMyMessage ? "font-bold" :"text-[#84d9d5]"} text-sm`}>{typing ? "Typing.." : last_message_text }</p>
           </div>
         </div>
         {
